715f3dd6678c5e1136a5c4863c6d5c4376867ab1
LameesMomen <lameeszakialabd@gmail.com>
2023-11-16 11:44:11 +0200

    finish client

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { flush } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { MessageService } from 'primeng/api';
 import { HttpHelperService } from 'src/app/core/services/http-helper/http-helper.service';
+import { ToastersService } from 'src/app/core/services/toaster/toasters.service';
 
 @Component({
   selector: 'app-client',
@@ -11,11 +14,23 @@ import { HttpHelperService } from 'src/app/core/services/http-helper/http-helper
 export class ClientComponent implements OnInit {
 
   AllclientsData:any
+  filterClients:any
+  active:boolean = false
   page:number = 1
 
-  constructor(private http : HttpHelperService , private spinner : NgxSpinnerService , private messageService : MessageService){}
+  constructor(private http : HttpHelperService , private spinner : NgxSpinnerService , private messageService : MessageService,private toasters : ToastersService,private  router : Router){}
   ngOnInit(): void {
    this.getClientsData()
+  }
+
+  RadioFilter(value:any){
+    this.active = true
+    if(value == 'active'){
+      this.filterClients = this.AllclientsData.filter((item:any) => item.is_approved);
+    }
+    else{
+      this.filterClients = this.AllclientsData.filter((item:any) => !item.is_approved);
+    }
   }
 
   getClientsData(){
@@ -31,5 +46,82 @@ export class ClientComponent implements OnInit {
       }
     )
   }
+
+  change_is_approved(item:any){
+    if(item.is_approved){
+      this.toasters.confirmationToaster({
+        title: 'هل انت متأكد !',
+  
+        text: `هل تريد تعليق ${item.first_name} ${item.last_name} ؟`,
+  
+        icon: 'question',
+  
+        confirmFunc: () => {
+          let payload: any = { ...item };
+  
+          payload.is_approved = !item.is_approved;
+  
+          this.showBannerAPI(payload);
+        },
+  
+        onDismiss: () => {
+  
+        },
+      });
+    }
+    else{
+      this.toasters.confirmationToaster({
+        title: 'هل انت متأكد !',
+  
+        text: `هل تريد تفعيل ${item.first_name} ${item.last_name} ؟`,
+  
+        icon: 'question',
+  
+        confirmFunc: () => {
+          let payload: any = { ...item };
+  
+          payload.is_approved = !item.is_approved;
+  
+          this.showBannerAPI(payload);
+        },
+  
+        onDismiss: () => {
+  
+        },
+      });
+    }
+
+
+}
+
+showBannerAPI(body: any) {
+  this.http
+    .put(`/auth/admin/client/edit/${body.id}`, {
+      is_approved: body.is_approved,
+    })
+    .subscribe(
+      (res) => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'تم',
+          detail: ' تنفيذ العملية بنجاح',
+        });
+        setTimeout(() => {
+          this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+            this.router.navigate(['client']);
+        });
+        }, 500);
+        this.spinner.hide();
+      },
+      (err) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'خطأ',
+          detail: 'حدث خطأ ما',
+        });
+        this.spinner.hide();
+      }
+    );
+}
 
 }
--- src/app/pages/client/components/view-personal-info/view-personal-info.component.ts
+++ src/app/pages/client/components/view-personal-info/view-personal-info.component.ts
@@ -16,6 +16,8 @@ export class ViewPersonalInfoComponent implements OnInit{
   radioValue: string ='personal';
   editMode : boolean =false;
   clientDetail : any
+  productImage :any='assets/profilePlaceholder.png'
+  imageFile :any
 
   constructor(private http : HttpHelperService,private spinner : NgxSpinnerService, private messageService : MessageService , private router : Router,private route : ActivatedRoute){
     this.id = this.route.snapshot.paramMap.get('id')
@@ -27,14 +29,50 @@ export class ViewPersonalInfoComponent implements OnInit{
 
   getData(){
     this.spinner.show();
-    this.http.get(`/auth/admin/client/list/?mobile=${this.id}`).subscribe(
+    this.http.get(`/auth/admin/client/list/${this.id}`).subscribe(
       (res:any)=>{
-        this.clientDetail=res[0];
+        this.clientDetail=res;
         this.spinner.hide();
       },
       err  =>{
         this.spinner.hide();
         this.messageService.add({severity:'error', summary:'خطأ', detail:'حدث خطأ ما'});
+      }
+    )
+  }
+
+  changeImage(event: any) {
+    this.imageFile = event.target.files[0];
+    this.productImage = this.imageFile ? URL.createObjectURL(this.imageFile) : this.clientDetail?.avatar;
+  }
+
+  submit(form:any){
+    this.spinner.show();
+
+    const body = new FormData();
+
+    body.append('first_name', form.first_name);
+    body.append('last_name', form.last_name);
+    body.append('mobile', form.mobile);
+    body.append('avatar', form.avatr ? form.avatar : this.imageFile);
+    body.append('city', form.city);
+    body.append('neighborhood', form.neighborhood);
+    body.append('street', form.street);
+    body.append('postal_code', form.postal_code);
+    body.append('mailbox', form.mailbox);
+
+
+    this.http.put(`/auth/admin/client/edit/${form.id}`,body).subscribe(
+      res=>{
+        this.spinner.hide();
+        this.messageService.add({severity:'success',summary:'تم', detail:' تنفيذ العملية بنجاح'});
+        setTimeout(() => {
+          this.editMode = ! this.editMode
+        }, 500);
+      },
+      err=>{
+        this.spinner.hide();
+        this.messageService.add({severity:'error',summary:'خطأ', detail:'حدث خطأ ما'});
       }
     )
   }
